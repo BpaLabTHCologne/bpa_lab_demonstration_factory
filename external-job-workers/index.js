957b60ce3cc7ee04c807a3b8698997d1930a8d84
Rahib Nazir Butt <rahibbutt@gmail.com>
2023-12-09 20:07:04 +0100

    Created monolithic worker NodeJS application

--- external-job-workers/index.js
+++ external-job-workers/index.js
@@ -48,3 +48,32 @@ connection.query('INSERT INTO `customer_order` (`id`, `name`, `email`, `phone`, 
 
   return job.complete(updateToBrokerVariables);
 }
+
+
+// Define an async main function to deploy a process, create a process instance, and log the outcome
+async function main() {
+  // Deploy the 'new-customer.bpmn' process
+  const res = await zbc.deployProcess('../Bicycle BPMN Model/bicycle-process-model.bpmn');
+  // Log the deployment result
+  console.log('Deployed process:', JSON.stringify(res, null, 2));
+
+  // Create a process instance of the 'new-customer-process' process, with a customerId variable set
+  // 'createProcessInstanceWithResult' awaits the outcome
+  const outcome = await zbc.createProcessInstanceWithResult({
+      bpmnProcessId: 'order-management-id',
+      variables: { 
+        name: "Rahib",
+        email: "test@gmail.com",
+        phone: "+491515151515",
+        address: "221B Baker Street",
+        product: "Mountain Bike",
+        quantity: 5,
+        orderStatus: "ORDER_IN_PROGRESS",
+      }
+  });
+  // Log the process outcome
+  console.log('Process outcome', JSON.stringify(outcome, null, 2));
+}
+
+// Call the main function to execute the script
+main();
