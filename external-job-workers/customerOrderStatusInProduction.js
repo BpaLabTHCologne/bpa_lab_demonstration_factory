a8f990863af1874fe499c192bee24cd31436ccc0
Rahib Nazir Butt <rahibbutt@gmail.com>
2024-01-10 18:28:54 +0100

    Implemented customerOrderStatusInProduction job worker

---
+++ external-job-workers/customerOrderStatusInProduction.js
@@ -0,0 +1,49 @@
+const ZB = require('zeebe-node')
+const mysql = require('mysql');
+
+const zbc = new ZB.ZBClient();
+
+//External job worker to store customer order
+const customerOrderStatusInProduction = zbc.createWorker({
+  taskType: 'customerOrderStatusInProduction',
+  taskHandler: handler,
+  debug: true,
+  loglevel: 'INFO',
+  onReady: () => customerOrderStatusInProduction.log('Job worker started successfully!')
+});
+
+function handler(job) {
+	// customerOrderStatusInProduction.log('Task variables', job.variables)
+    try {
+        const orderID = parseInt(job.variables.orderID)
+        var connection = mysql.createConnection({
+            connectionLimit: 10,
+            host: process.env.MYSQL_HOST_NAME,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+            port: process.env.MYSQL_HOST_PORT,
+        });
+    
+        connection.connect();
+    
+        connection.query('UPDATE `customer_order` SET `orderStatus` = "ORDER_IN_PRODUCTION" WHERE `customer_order`.`id` = ' + orderID + ';',
+          async function (error, results, fields) {
+            if (error) throw error;
+            console.log('Results: ', JSON.stringify(results));
+          });
+    
+        
+    
+      } catch (error) {
+        console.log(error)
+      }
+
+      const updateToBrokerVariables = {
+        orderStatus: 'ORDER_IN_PRODUCTION',
+	}
+  connection.end();
+	return job.complete(updateToBrokerVariables)
+}
+
+  module.exports = customerOrderStatusInProduction;
