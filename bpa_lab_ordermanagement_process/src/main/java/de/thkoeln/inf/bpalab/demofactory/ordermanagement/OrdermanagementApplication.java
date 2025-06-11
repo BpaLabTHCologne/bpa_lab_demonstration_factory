a71ac39cb9f1c8df11916e35dd614bd7f2ff9d1b
Uwe Poborski <uwe.poborski@th-koeln.de>
2025-06-11 09:36:01 +0200

    improved fix app only running in intellij

--- bpa_lab_ordermanagement_process/src/main/java/de/thkoeln/inf/bpalab/demofactory/ordermanagement/OrdermanagementApplication.java
+++ bpa_lab_ordermanagement_process/src/main/java/de/thkoeln/inf/bpalab/demofactory/ordermanagement/OrdermanagementApplication.java
@@ -1,14 +1,10 @@
 package de.thkoeln.inf.bpalab.demofactory.ordermanagement;
 
-import io.camunda.zeebe.client.ZeebeClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class OrdermanagementApplication {
 
     public static void main(final String[] args) {
