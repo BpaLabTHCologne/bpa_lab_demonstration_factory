117697cc410bdedb69ff45d626577dd649287fc6
Uwe Poborski <uwe.poborski@th-koeln.de>
2025-07-23 16:07:21 +0200

    cleaning bpa_lab_manufacturing_process;update sql-script for bpa_lab_purchasing_process

--- bpa_lab_manufacturing_process/src/main/java/de/thkoeln/inf/bpalab/ftfactory/zeebemqttbridge/publisher/IPublisher.java
+++
@@ -1,9 +0,0 @@
-package de.thkoeln.inf.bpalab.ftfactory.zeebemqttbridge.publisher;
-
-import de.thkoeln.inf.bpalab.ftfactory.zeebemqttbridge.mqtt.TopicPayload;
-
-public interface IPublisher {
-
-	TopicPayload getTopicPayload();
-
-}
--- bpa_lab_purchasing_process/src/provision/createVendor.sql
+++ bpa_lab_purchasing_process/src/provision/createVendor.sql
@@ -1,26 +1,46 @@
-create table if not exists vendor
-(
-    vendor_name    varchar(255) not null
-        primary key,
-    vendor_contact varchar(255) null
+use bpa_lab_demonstration_factory;
+
+create table if not exists
+    vendor (
+        vendor_name     varchar(255) not null
+                        primary key,
+        vendor_contact  varchar(255) null
 );
 
-create table if not exists vendor_bikecomponent
-(
-    vendor_name         varchar(255) not null,
-    bikecomponent_title varchar(255) not null,
-    price               double       null,
-    primary key (vendor_name, bikecomponent_title),
-    constraint FKiih35bm2koc68i9y3ji8ubmcx
-        foreign key (vendor_name) references vendor (vendor_name),
-    constraint FKm6qiubla97d6mihosgnkgf7co
-        foreign key (bikecomponent_title) references bike_component (title)
+create table if not exists
+    vendor_bikecomponent (
+        vendor_name         varchar(255) not null,
+        bikecomponent_title varchar(255) not null,
+        price               double       null,
+        primary key (vendor_name, bikecomponent_title),
+        constraint FKiih35bm2koc68i9y3ji8ubmcx
+            foreign key (vendor_name) references vendor (vendor_name),
+        constraint FKm6qiubla97d6mihosgnkgf7co
+            foreign key (bikecomponent_title) references bike_component (title)
 );
 
-insert into vendor values ("Hans Hansen Comps", "hans@hansen.de");
-insert into vendor values ("Paulsen Bike Componente", "paul@paulsen.it")
+insert into vendor
+    values ("Hans Hansen Comps",
+            "hans@hansen.de"
+    );
+insert into vendor
+    values ("Paulsen Bike Componente",
+            "paul@paulsen.it"
+    );
 
-insert into vendor_bike_component values ("Hans Hansen Comps", "Citybike Component Kit", 111.11)
-insert into vendor_bike_component values ("Paulsen Bike Componente", "Citybike Component Kit", 122.22)
-insert into vendor_bike_component values ("Paulsen Bike Componente", "Lastenrad Component Kit", 145)
-insert into vendor_bike_component values ("Paulsen Bike Componente", "Mountainbike Component Kit", 130)
+insert into vendor_bike_component
+       values ("Hans Hansen Comps",
+           "Citybike Component Kit", 111.11
+    );
+insert into vendor_bike_component
+    values ("Paulsen Bike Componente",
+        "Citybike Component Kit", 122.22
+    );
+insert into vendor_bike_component
+    values ("Paulsen Bike Componente",
+        "Lastenrad Component Kit", 145
+    );
+insert into vendor_bike_component
+    values ("Paulsen Bike Componente",
+            "Mountainbike Component Kit", 130
+    );
