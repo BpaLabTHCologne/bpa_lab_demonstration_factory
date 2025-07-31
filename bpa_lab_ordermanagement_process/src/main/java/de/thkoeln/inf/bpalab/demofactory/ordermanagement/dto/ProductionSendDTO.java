6270ea0cb759a74b221aa7e3eb7eeb8e9240fec4
Uwe Poborski <uwe.poborski@th-koeln.de>
2025-07-31 10:33:57 +0200

    further decoupling process applications in entity, repository, service layer

---
+++ bpa_lab_ordermanagement_process/src/main/java/de/thkoeln/inf/bpalab/demofactory/ordermanagement/dto/ProductionSendDTO.java
@@ -0,0 +1,8 @@
+package de.thkoeln.inf.bpalab.demofactory.ordermanagement.dto;
+
+import de.thkoeln.inf.bpalab.demofactory.common.dto.OrderItemDTO;
+
+public class ProductionSendDTO {
+    public String orderNumber;
+    public OrderItemDTO produceBikeModel;
+}
