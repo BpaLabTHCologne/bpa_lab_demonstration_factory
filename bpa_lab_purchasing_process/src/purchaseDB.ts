df03fd6a4e13b909e51efcf63d407eaa700d1e7f
Uwe Poborski <uwe.poborski@th-koeln.de>
2026-08-11 15:59:55 +0200

    c89lite with mysql

--- bpa_lab_purchasing_process/src/purchaseDB.ts
+++ bpa_lab_purchasing_process/src/purchaseDB.ts
@@ -7,9 +7,9 @@ config()
 export let con = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
-    user: "root",
-    password: "P4ssword!",
-    database: "bpa_lab_demonstration_factory"
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PW,
+    database: process.env.MYSQL_DB
 });
 
 export function getPurchaseOrder(purchaseOrderNumber : string) {
